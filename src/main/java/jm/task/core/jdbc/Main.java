61c6f907bcf422f26d481dc59d4f2b764985568a
Leks3472 <ras.ufa@mail.ru>
2022-05-20 01:38:52 +0500

    1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 18);
+        userService.saveUser("Петр", "Петров", (byte) 25);
+        userService.saveUser("Сидор", "Сидоров", (byte) 45);
+        userService.saveUser("Дуня", "Толстая", (byte) 45);
+
+        userService.removeUserById(3);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+//https://www.youtube.com/watch?v=F1lQ7cE3nuE&list=PLIU76b8Cjem5qdMQLXiIwGLTLyUHkTqi2&index=4
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Or885522!";
+
+        public static Connection getConnection () {
+            Connection connection = null;
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+               // if (!connection.isClosed()) {
+                    System.out.println("Соединение с БД установлено Connection is OK");
+                //}
+                //connection.close();
+            } catch (SQLException e) {
+                System.out.println("Соединение с БД не установлено Connection not established");
+            }
+            return connection;
+        }
+
 }
