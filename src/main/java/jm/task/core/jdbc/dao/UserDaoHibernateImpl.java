4975390621107e6c09eb1508c9653352a7e06a5b
Leks3472 <ras.ufa@mail.ru>
2022-05-26 20:51:51 +0500

    1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -16,16 +16,19 @@ public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
 
     }
-
     @Override
     public void createUsersTable() {
         try (Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
-            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +
-                    "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), lastname VARCHAR(255), age INT)");
-                    //.executeUpdate();
+            session.createSQLQuery(""" 
+                    CREATE TABLE IF NOT EXISTS User (
+                    id INTEGER PRIMARY KEY AUTO_INCREMENT not NULL,
+                    name VARCHAR(255) not NULL,
+                    lastName VARCHAR(255) not NULL,
+                    age TINYINT not NULL)
+                    """).executeUpdate();
             transaction.commit();
-            //System.out.println("Таблица создана");
+            System.out.println("Таблица создана");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -35,30 +38,24 @@ public class UserDaoHibernateImpl implements UserDao {
     public void dropUsersTable() {
         try (Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
-            session.createSQLQuery("Drop table if exists test.MyTable").executeUpdate();
+            session.createSQLQuery("Drop table if exists User")
+                    .executeUpdate();
             transaction.commit();
-            System.out.println("Таблица удалена");
         } catch (Exception e) {
             e.printStackTrace();
-
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try
-        {Session session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             session.save(new User(name, lastName, age));
             transaction.commit();
             System.out.println("User с именем – " + name + " добавлен в базу данных");
         } catch (Exception e) {
             e.printStackTrace();
-
-        } finally {
-            sessionFactory.openSession();
         }
-
     }
 
     @Override
@@ -78,23 +75,20 @@ public class UserDaoHibernateImpl implements UserDao {
         List<User> users = new ArrayList<>();
         try (Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
-            users = session.createQuery(String.valueOf(User.class)).list();
+            users = session.createQuery("FROM User").list();
             transaction.commit();
         } catch (Exception e) {
             e.printStackTrace();
-            }
+        }
         return users;
     }
-
 
     @Override
     public void cleanUsersTable() {
         try (Session session = sessionFactory.openSession()) {
-            final List<User> instances = session.createSQLQuery(String.valueOf(User.class)).list();
-            for (Object o : instances) {
-                session.delete(o);
-            }
-            session.getTransaction().commit();
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("Drop table if exists User").executeUpdate();
+            transaction.commit();
             System.out.println("Таблица очищена");
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -12,7 +12,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String URL = "jdbc:mysql://localhost:3306/test2";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "Or885522!";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
